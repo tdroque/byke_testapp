5a017044b4709154ed820be0a45e779f445bc9b1
TL <t_laroque@fanshaweonline.ca>
2019-11-04 08:07:54 -0500

    Cleanup

--- byke_testapp.py
+++ byke_testapp.py
@@ -1,26 +1,43 @@
-# -----------------------------------------------------
+# --------------------------------------------
 # File: byke_testapp.py
+# Date: 30/09/2019
 # Author: Tanner L
-# Date: 09/18/19
-# Desc: Test app for byke project, functions to test gps functions, save data, 
-#		and read/write registers on microchips(motor controller pic and tail light pic)
-# -----------------------------------------------------
+# Modified:
+# Desc: Test application for testing of byke systems, gps, motion sensor, and pic communication.
+#       Setup for sql db testing.
+# --------------------------------------------
 import tkinter as tk
 import math
 import sqlite3
 import board
 
-conn = sqlite3.connect('byke_test.db')
+conn = sqlite3.connect('byke_testApp.db')
 
 print("Opened database successfully")
 
-conn.execute('''CREATE TABLE BYKEDATA
-         (ID INT PRIMARY KEY     NOT NULL,
-         TIME           TIME    NOT NULL,
-         SPEED          INT     NOT NULL,
-         LAT            CHAR(30),
-         LONG           CHAR(30),
-         LOCATION       CHAR(60));''')
+conn.execute('''CREATE TABLE IF NOT EXISTS TRIP_STATS
+        (TRIP_ID  INTEGER PRIMARY KEY NOT NULL,
+        TRIP_DATE          TEXT,
+        TRIP_TIME          INTEGER,
+        TRIP_MAXSPEED      REAL,
+        TRIP_AVGSPEED      REAL,
+        TRIP_DISTANCE      REAL,
+        TRIP_UPDISTANCE    REAL,
+        TRIP_DOWNDISTANCE  REAL);''')
+
+conn.execute('''CREATE TABLE IF NOT EXISTS GPS_DATA
+         (ENTRY_ID INT PRIMARY KEY     NOT NULL,
+         TIME           TEXT    NOT NULL,
+         SPEED          REAL,
+         LAT            REAL,
+         LNG            REAL,
+         ALT            REAL,
+         CLIMB          REAL,
+         XROT           REAL,
+         YROT           REAL,
+         TRIP_ID        INTEGER NOT NULL,
+         FOREIGN KEY (TRIP_ID)
+            REFERENCES TRIP_STATS (TRIP_ID) );''')
 
 print("Table created successfully")
 
@@ -49,74 +66,229 @@ i = 0
 global recordrunning
 recordrunning = 0
 
+global list1
+list1 = []
+
+global totaldistance
+totaldistance = 0
+
+global tripNum
+tripNum = 0
+
 # i2c addresses
 i2cBus = smbus.SMBus(1)  # Setup for i2c communication via smbus
 tailEndPicAddress = 0x55  # i2c address of tail end pic
 batteryPicAddress = 0x45  # i2c address of battery location pic
-headEndPicAddress = 0x35  # i2c address of head end pic
 
 motionAddress = 0x68  # address for mpu5060 motion sensor
 motionPowerMgmt1 = 0x6b  # memory location of power register
 motionPowerMgmt2 = 0x6c  # memory location of power register
 
 
-def temperatureread():
+# -----------------------------------------------------
+# Function: query_test
+# Author:
+# Modified: Tanner L
+# Date: 01/10/19
+# Desc: Test sql query
+# Inputs:
+# Outputs:
+# -----------------------------------------------------
+def query_test():
 
-    sensor = adafruit_dht.DHT11(board.D16)  # call library for DHT11 temperature sensor
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM TRIP_STATS WHERE TRIP_ID=?", (tripNum,))
 
-    temperature = sensor.temperature  # read in temperature and humidity
+    rows = cur.fetchall()
 
-    temperaturebutton.config(text=str(temperature))
+    conn.close()
+
+    for row in rows:
+        print(row)
 
 
+# -----------------------------------------------------
+# Function: temperature_read
+# Author:
+# Modified: Tanner L
+# Date: 01/10/19
+# Desc: Read temperature from DHT11 sensor
+# Inputs:
+# Outputs:
+# -----------------------------------------------------
+def temperature_read():
+
+    try:
+        sensor = adafruit_dht.DHT11(board.D16)  # call library for DHT11 temperature sensor
+
+        temperature = sensor.temperature  # read in temperature and humidity
+
+        temperaturebutton.config(text=str(temperature))
+    except RuntimeError as e:
+        print('temp error {}'.format(e.args))
+
+
+# -----------------------------------------------------
+# Function: record
+# Author:
+# Modified: Tanner L
+# Date: 01/10/19
+# Desc: Start and Stop recording
+# Inputs:
+# Outputs:
+# -----------------------------------------------------
+def record():
+
+    global tripNum
+    global i
+    global recordrunning
+
+    if recordrunning == 1:
+        recordrunning = 0
+        recordbutton.config(text="Record")
+
+    else:
+        recordrunning = 1
+
+        recordbutton.config(text="Recording")
+
+        conn = sqlite3.connect('byke_testApp.db')
+
+        cur = conn.cursor()
+
+        cur.execute("SELECT ENTRY_ID, TRIP_ID FROM GPS_DATA WHERE ENTRY_ID = (SELECT MAX(ENTRY_ID) FROM GPS_DATA)")
+
+        max_entry = cur.fetchone()
+
+        conn.close()
+
+        try:
+            i = max_entry[0] + 1
+        except:
+            i = 0
+
+        try:
+            tripNum = max_entry[1] + 1
+        except:
+            tripNum = 1
+
+
+# -----------------------------------------------------
+# Function: gps
+# Author:
+# Modified: Tanner L
+# Date: 01/10/19
+# Desc: Query gps module and save to database
+# Inputs:
+# Outputs:
+# -----------------------------------------------------
 def gps():
 
     global i
     global recordrunning
+    global totaldistance
+    global list1
 
     gpsd.connect()
     gpsData = gpsd.get_current()
 
-    if recordrunning == 1:
-        recordrunning = 0
-    else:
-        recordrunning = 1
-
     if gpsData.mode > 1:
+
         gpsTime = gpsData.time
-        gpsSpeed = gpsData.speed()
+        speed = gpsData.hspeed
         gpsLat = gpsData.lat
         gpsLong = gpsData.lon
-        gpsLoc = gpsData.position()
-        timelist = list(gpsData.time[11:13])
+        gpsAlt = gpsData.alt
+        gpsClimb = gpsData.climb
 
-        gpsbutton.config(text='Fix')
+        speed = speed * 3.6
 
-        conn = sqlite3.connect('byke_test.db')
+        gpsbutton.config(text='Fix ' + str(i))
 
-        conn.execute("INSERT INTO BYKEDATA (ID,TIME,SPEED,LAT,LONG,LOCATION) \
-              VALUES (i, gpsTime, gpsSpeed, gpsLat , gpsLong )")
+        if recordrunning == 1:
 
-        conn.commit()
+            xrotate, yrotate = motion()
 
-        conn.close()
+            list1.append((i, str(gpsTime), speed, gpsLat, gpsLong, gpsAlt,
+                         gpsClimb, xrotate, yrotate, tripNum))
+            i += 1
 
-        i = i + 1
+            speed = float(speed)
 
-    gpsbutton.config(text='NO Fix')
+            if speed > 0.5:
+                distance = speed / 3600
+                totaldistance = totaldistance + distance
+
+                print("Speed: {} Distance: {} Total Distance: {}".format(speed, distance, totaldistance))
+
+        else:
+            try:
+                conn = sqlite3.connect('byke_testApp.db')
+
+                c = conn.cursor()
+
+                entry = "INSERT INTO GPS_DATA (ENTRY_ID, TIME, SPEED, LAT, LNG, ALT, CLIMB, TRIP_ID) \
+                      VALUES (?, ?, ?, ? ,?, ?, ?, ?)"
+
+                c.executemany(entry, list1)
+
+                conn.commit()
+
+                c.execute("SELECT MAX(SPEED) FROM GPS_DATA")
+                maxspeed = c.fetchone()
+                print(maxspeed)
+
+                c.execute("SELECT AVG(SPEED) FROM GPS_DATA")
+                avgspeed = c.fetchone()
+                print(avgspeed)
+
+                listStats = ( (0, 0, maxspeed, avgspeed, totaldistance, 0, 0, tripNum) )
+
+                entry2 = "INSERT INTO TRIP_STATS (TRIP_TIME, TRIP_DATE, TRIP_MAXSPEED, TRIP_AVGSPEED, TRIP_DISTANCE, " \
+                         "TRIP_UPDISTANCE, TRIP_DOWNDISTANCE, TRIP_ID)" \
+                         " VALUES (?, ?, ?, ?, ?, ?, ?, ?)"
+
+                c.executemany(entry2, listStats)
+
+                conn.commit()
+
+                conn.close()
+            except:
+                pass
+
+    else:
+        gpsbutton.config(text='NO Fix')
 
     if recordrunning == 1:
         gpsbutton.after(1000, gps)
 
 
-def read_word(adr):  # function for reading motion sensor data
+# -----------------------------------------------------
+# Function: read_word
+# Author:
+# Modified: Tanner L
+# Date: 01/10/19
+# Desc: Combine two register from motion sensor
+# Inputs:
+# Outputs:
+# -----------------------------------------------------
+def read_word(adr):
     high = i2cBus.read_byte_data(motionAddress, adr)
     low = i2cBus.read_byte_data(motionAddress, adr + 1)
     val = (high << 8) + low
     return val
 
 
-def readWordMotion(adr):  # function for calculating motion sensor data
+# -----------------------------------------------------
+# Function: read_word_motion
+# Author:
+# Modified: Tanner L
+# Date: 01/10/19
+# Desc: Adjust value from motion sensor
+# Inputs:
+# Outputs:
+# -----------------------------------------------------
+def read_word_motion(adr):
     val = read_word(adr)
     if val >= 0x8000:
         return -((65535 - val) + 1)
@@ -124,100 +296,141 @@ def readWordMotion(adr):  # function for calculating motion sensor data
         return val
 
 
-def motion():  # function for communicating with motion sensor, mpu5060
+# -----------------------------------------------------
+# Function: motion
+# Author:
+# Modified: Tanner L
+# Date: 01/10/19
+# Desc: Query motion sensor
+# Inputs:
+# Outputs:
+# -----------------------------------------------------
+def motion():
 
     i2cBus.write_byte_data(motionAddress, motionPowerMgmt1, 0)
-    accel_xout_scaled = readWordMotion(0x3b) / 16384.0
-    accel_yout_scaled = readWordMotion(0x3d) / 16384.0
-    accel_zout_scaled = readWordMotion(0x3f) / 16384.0
+    accel_xout_scaled = read_word_motion(0x3b) / 16384.0
+    accel_yout_scaled = read_word_motion(0x3d) / 16384.0
+    accel_zout_scaled = read_word_motion(0x3f) / 16384.0
     yRotate = -math.degrees(math.atan2(accel_xout_scaled, (math.sqrt((accel_yout_scaled * accel_yout_scaled) +
-                                                                             (accel_zout_scaled * accel_zout_scaled)))))
+                                                          (accel_zout_scaled * accel_zout_scaled)))))
     xRotate = -math.degrees(math.atan2(accel_yout_scaled, (math.sqrt((accel_xout_scaled * accel_xout_scaled) +
-                                                                             (accel_zout_scaled * accel_zout_scaled)))))
+                                                          (accel_zout_scaled * accel_zout_scaled)))))
+
+    motionbutton.config(text=str(round(yRotate, 2) + ' ' + str(round(xRotate, 2))))
+
+    return xRotate, yRotate
 
 
-def headlight():  # function for handling button presses
+# -----------------------------------------------------
+# Function: send_tail
+# Author:
+# Modified: Tanner L
+# Date: 01/10/19
+# Desc: Send value to tail end pic
+# Inputs:
+# Outputs:
+# -----------------------------------------------------
+def send_tail():
 
-    global headlightpressed
-
-    if headlightpressed == 0:  # headlight button
-        i2cBus.write_byte_data(tailEndPicAddress, 2, True)
-        i2cBus.write_byte_data(tailEndPicAddress, 3, True)
-
-    else:
-        i2cBus.write_byte_data(tailEndPicAddress, 2, False)
-        i2cBus.write_byte_data(tailEndPicAddress, 3, False)
-
-
-def rightturn():
-
-    global rightpressed
-
-    if leftpressed == 0:  # left signal button
-        i2cBus.write_byte_data(tailEndPicAddress, 1, True)
-        rightpressed = 1
-
-    else:
-        rightpressed = 0
-        i2cBus.write_byte_data(tailEndPicAddress, 1, False)
+    i2cBus.write_byte_data(tailEndPicAddress, int(regspinner.get()), int(regvaluespinner.get()))
 
 
-def leftturn():
+# -----------------------------------------------------
+# Function: send_motor
+# Author:
+# Modified: Tanner L
+# Date: 01/10/19
+# Desc: Send value to motor pic
+# Inputs:
+# Outputs:
+# -----------------------------------------------------
+def send_motor():
 
-    global leftpressed
-
-    if leftpressed == 0:  # left signal button
-        i2cBus.write_byte_data(tailEndPicAddress, 0, True)
-        leftpressed = 1
-
-    else:
-        leftpressed = 0
-        i2cBus.write_byte_data(tailEndPicAddress, 0, False)
+    i2cBus.write_byte_data(batteryPicAddress, int(regspinner.get()), int(regvaluespinner.get()))
 
 
-def brake():
+# -----------------------------------------------------
+# Function: read_motor
+# Author:
+# Modified: Tanner L
+# Date: 01/10/19
+# Desc: Read value from motor pic
+# Inputs:
+# Outputs:
+# -----------------------------------------------------
+def read_motor():
 
-    global brakepressed
+    motorrec = i2cBus.read_byte_data(batteryPicAddress, int(regspinner.get()))
+    regvaluespinner.delete(0, 'end')
+    regvaluespinner.insert(0, motorrec)
 
-    if brakepressed == 0:  # brake signal button
-        brakepressed = 1
-        i2cBus.write_byte_data(tailEndPicAddress, 5, True)
-    else:
-        brakepressed = 0
-        i2cBus.write_byte_data(tailEndPicAddress, 5, False)
+
+# -----------------------------------------------------
+# Function: read_tail
+# Author:
+# Modified: Tanner L
+# Date: 01/10/19
+# Desc: Read value from tail end pic
+# Inputs:
+# Outputs:
+# -----------------------------------------------------
+def read_tail():
+
+    tailrec = i2cBus.read_byte_data(tailEndPicAddress, int(regspinner.get()))
+    regvaluespinner.delete(0, 'end')
+    regvaluespinner.insert(0, tailrec)
 
 
 mainWindow = tk.Tk()
 mainWindow.title('Byke')
-mainWindow.geometry('480x300+0+0')
+mainWindow.geometry('400x250+0+0')
 mainWindow.columnconfigure(0, weight=1)
 mainWindow.columnconfigure(1, weight=1)
 mainWindow.columnconfigure(2, weight=1)
 mainWindow.rowconfigure(0, weight=1)
 mainWindow.rowconfigure(1, weight=1)
 mainWindow.rowconfigure(2, weight=1)
+mainWindow.rowconfigure(3, weight=1)
 mainWindow.config(bg='white')
 
-leftturnbutton = tk.Button(mainWindow, text='Left Turn', borderwidth=2, command=leftturn)  # left turn signal button
-leftturnbutton.grid(row=0, column=0, sticky='nswe')
-
-rightturnbutton = tk.Button(mainWindow, text='Right Turn', borderwidth=2, command=rightturn)  # right turn signal button
-rightturnbutton.grid(row=0, column=1, sticky='nswe')
-
-brakebutton = tk.Button(mainWindow, text='Brake', borderwidth=2, command=brake)  # brake button
-brakebutton.grid(row=0, column=2, sticky='nswe')
-
-headlightbutton = tk.Button(mainWindow, text='Headlight', borderwidth=2, command=headlight)  # headlight button
-headlightbutton.grid(row=1, column=0, sticky='nswe')
-
-gpsbutton = tk.Button(mainWindow, text='gps', borderwidth=2, command=gps)  # headlight button
-gpsbutton.grid(row=2, column=0, sticky='nswe')
+gpsbutton = tk.Button(mainWindow, text='gps', borderwidth=2, command=gps)
+gpsbutton.grid(row=0, column=0, sticky='nswe')
 
 if recordrunning == 1:
     gpsbutton.after(1000, gps)
 
-temperaturebutton = tk.Button(mainWindow, text='temp', borderwidth=2, command=temperatureread)  # headlight button
-temperaturebutton.grid(row=2, column=1, sticky='nswe')
+temperaturebutton = tk.Button(mainWindow, text='temperature', borderwidth=2, command=temperature_read)
+temperaturebutton.grid(row=2, column=0, sticky='nswe')
+
+motionbutton = tk.Button(mainWindow, text='motion', borderwidth=2, command=motion)
+motionbutton.grid(row=3, column=0, sticky='nswe')
+
+recordbutton = tk.Button(mainWindow, text='record', borderwidth=2, command=record)
+recordbutton.grid(row=1, column=0, sticky='nswe')
+
+reglabel = tk.Label(mainWindow, text='Register')
+reglabel.grid(row=0, column=1, sticky='nsew')
+
+regspinner = tk.Spinbox(mainWindow, width=3, from_=0, to=12, font=(None, 18))
+regspinner.grid(row=1, column=1, sticky='nswe')
+
+tailsendbutton = tk.Button(mainWindow, text='Send to Tail', borderwidth=2, command=send_tail)
+tailsendbutton.grid(row=0, column=2, sticky='nswe')
+
+motorsendbutton = tk.Button(mainWindow, text='Send to Motor', borderwidth=2, command=send_motor)
+motorsendbutton.grid(row=1, column=2, sticky='nswe')
+
+regvalue = tk.Label(mainWindow, text='REG Value')
+regvalue.grid(row=2, column=1, sticky='nsew')
+
+tailreadbutton = tk.Button(mainWindow, text='Read Tail', borderwidth=2, command=read_tail)
+tailreadbutton.grid(row=2, column=2, sticky='nswe')
+
+motorreadbutton = tk.Button(mainWindow, text='Read Motor', borderwidth=2, command=read_motor)
+motorreadbutton.grid(row=3, column=2, sticky='nswe')
+
+regvaluespinner = tk.Spinbox(mainWindow, width=3, from_=0, to=100, font=(None, 18))
+regvaluespinner.grid(row=3, column=1, sticky='nswe')
 
 mainWindow.mainloop()
 
